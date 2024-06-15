bea64cebd330de009b27b30e8a10bad9c0ec5341
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-06-15 19:53:55 +0600

    Project initialized

---
+++ client/src/layouts/Main.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+    return (
+        <div>
+            {/* Navbar */}
+                <h1>MAin</h1>
+            {/* Outlet */}
+
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Main;
